ab12bcbeedf37b15ee020312484118d34d85a8ac
Deiximar Virriel <deiximar@gmail.com>
2024-07-24 15:03:17 +0200

    Delete connection funcion in app

--- src/main/java/com/library/App.java
+++ src/main/java/com/library/App.java
@@ -1,15 +1,18 @@
 package com.library;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.Scanner;
-
-import io.github.cdimascio.dotenv.Dotenv;
 
 public class App {
     public static void main(String[] args) {
+
+        BookDAO model = new BookDAO();
+
+        Book book = new Book("Una historia de Shimotsuma",
+                "Shimotsuma Monogatari sigue a Miko, una chica lolita, y Yuichi, un joven común, en Shimotsuma. A pesar de sus diferencias, desarrollan una amistad profunda que les ayuda a crecer y comprenderse.",
+                "9788494746017");
+
+        model.addBook(book);
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\nBiblioteca de todos\n¿Qué desea realizar? (Selecione un número)");
@@ -41,33 +44,23 @@ public class App {
                     System.out.println("Seleccione un numero del 1 - 6\n");
                     option = scanner.nextInt();
             }
+
+            // Statement statement = connection.createStatement();
+            // ResultSet resultSet = statement.executeQuery("SELECT * FROM books");
+
+            // while (resultSet.next()) {
+            // int id = resultSet.getInt("id_book");
+            // String title = resultSet.getString("title");
+            // String description = resultSet.getString("description");
+            // String ISBN_code = resultSet.getString("isbn_code");
+
+            // System.out.println("id: " + id + ", titulo: " + title + ", descripcion: " +
+            // description
+            // + ", codigo ISBN: " + ISBN_code);
+            // }
         }
 
         scanner.close();
-        Dotenv dotenv = Dotenv.load();
-
-        String username = dotenv.get("DB_USER");
-        String password = dotenv.get("DB_PASSWORD");
-        String bdName = "library";
-        String url = "jdbc:postgresql://localhost:5432/" + bdName;
-
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            System.out.println("Conectado a la base de datos de PostgreSQL");
-
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM books");
-
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id_book");
-                String title = resultSet.getString("title");
-                String description = resultSet.getString("description");
-                String ISBN_code = resultSet.getString("isbn_code");
-
-                System.out.println("id: " + id + ", titulo: " + title + ", descripcion: " + description
-                        + ", codigo ISBN: " + ISBN_code);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
+
 }
