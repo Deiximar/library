e302355f8757cbac1f47c480f674a2a01e3c1710
Deiximar Virriel <deiximar@gmail.com>
2024-08-01 14:59:34 +0200

    Add logic to edit auhors in a book

--- src/main/java/com/library/model/AuthorBookDAO.java
+++ src/main/java/com/library/model/AuthorBookDAO.java
@@ -2,6 +2,9 @@ package com.library.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.library.config.DBManager;
 
@@ -46,14 +49,75 @@ public class AuthorBookDAO implements AuthorBookDAOInterface {
     String query = "DELETE FROM authors_books WHERE id_book = ?";
     int resultRowsDeleted = 0;
     try {
-        connect = DBManager.initConnection();
-        preparedStatement = connect.prepareStatement(query);
+      connect = DBManager.initConnection();
+      preparedStatement = connect.prepareStatement(query);
 
-        preparedStatement.setInt(1, id);
-        resultRowsDeleted = preparedStatement.executeUpdate();
+      preparedStatement.setInt(1, id);
+      resultRowsDeleted = preparedStatement.executeUpdate();
     } catch (Exception e) {
-        System.out.println(e.getMessage());
+      System.out.println(e.getMessage());
     }
     return resultRowsDeleted;
-}
+  }
+
+  public int deleteAuthorBookByBookId(int bookId, int authorId) {
+    String query = "DELETE FROM authors_books WHERE id_book = ? AND id_author = ?";
+    int resultRowsDeleted = 0;
+    try {
+      connect = DBManager.initConnection();
+      preparedStatement = connect.prepareStatement(query);
+
+      preparedStatement.setInt(1, bookId);
+      preparedStatement.setInt(2, authorId);
+      resultRowsDeleted = preparedStatement.executeUpdate();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return resultRowsDeleted;
+  }
+
+  public List<Author> getAuthorsByBookId(int bookId) {
+    List<Author> authors = new ArrayList<>();
+
+    ResultSet resultSet = null;
+    try {
+      connect = DBManager.initConnection();
+      if (connect != null) {
+        String selectQuery = "SELECT * FROM authors a JOIN authors_books ab ON ab.id_author = a.id_author WHERE ab.id_book = ?";
+        preparedStatement = connect.prepareStatement(selectQuery);
+        preparedStatement.setInt(1, bookId);
+        resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+          Author author = new Author();
+          author.setIdAuthor(resultSet.getInt("id_author"));
+          author.setName(resultSet.getString("name"));
+          author.setLastName(resultSet.getString("last_name"));
+          authors.add(author);
+        }
+      } else {
+        System.out.println("Conexión a la base de datos fallida");
+      }
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+
+    } finally {
+      try {
+        if (resultSet != null) {
+          resultSet.close();
+        }
+        if (preparedStatement != null) {
+          preparedStatement.close();
+        }
+        if (connect != null) {
+          connect.close();
+        }
+      } catch (Exception e) {
+        System.out.println(e.getMessage());
+      }
+    }
+    return authors;
+
+  }
 }
--- src/main/java/com/library/model/AuthorBookDAOInterface.java
+++ src/main/java/com/library/model/AuthorBookDAOInterface.java
@@ -1,6 +1,14 @@
 package com.library.model;
 
+import java.util.List;
+
 public interface AuthorBookDAOInterface {
   boolean addAuthorBook(int authorId, int bookId);
+
   int deleteAuthorBookByBookId(int id);
+
+  List<Author> getAuthorsByBookId(int bookId);
+
+  int deleteAuthorBookByBookId(int idBook, int idAuthor);
+
 }
